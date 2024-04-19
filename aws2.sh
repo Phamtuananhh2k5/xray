1f4124feaba3c230bd1a5c3ae651244cb90b44f5
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-04-19 16:03:01 +0700

    Update aws2.sh

--- aws2.sh
+++ aws2.sh
@@ -5,10 +5,10 @@ read option
 option=$(echo "$option" | tr '[:upper:]' '[:lower:]')  # Chuyển đổi thành chữ thường
 
 case $option in
-  a) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws1.sh);;
-  b) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws2.sh);;
-  c) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws3.sh);;
-  d) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws4.sh);;
-  e) bash <(curl -Ls https://raw.githubusercontent.com/Panhuqusyxh/xrayra/master/aws5.sh);;
+  a) bash <(curl -Ls https://raw.githubusercontent.com/Phamtuananhh2k5/xrayra/master/aws1.sh);;
+  b) bash <(curl -Ls https://raw.githubusercontent.com/Phamtuananhh2k5/xrayra/master/aws2.sh);;
+  c) bash <(curl -Ls https://raw.githubusercontent.com/Phamtuananhh2k5/xrayra/master/aws3.sh);;
+  d) bash <(curl -Ls https://raw.githubusercontent.com/Phamtuananhh2k5/xrayra/master/aws4.sh);;
+  e) bash <(curl -Ls https://raw.githubusercontent.com/Phamtuananhh2k5/xrayra/master/aws5.sh);;
   *) echo "Lỗi yêu cầu kiểm tra lại"; exit 1;;
 esac
