03a9e8d223fb8790e3b9eb4fd5b7f12e9fab2856
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-05-05 15:32:09 +0700

    Update gost-out.sh

--- gost-out.sh
+++ gost-out.sh
@@ -31,12 +31,15 @@ sudo chmod 777 gost_auto.sh
 
 # Ghi tác vụ cron đầu tiên vào tệp /root/cloudflare_cron
 echo "*/1 * * * * /usr/local/bin/cloudflare-ddns --update-now >> /root/ipcf.log 2>&1" > /root/cloudflare_cron
-echo "*/1 * * * * /root/gost_auto.sh" > /root/cloudflare_cron
+
 # Ghi tác vụ cron thứ hai vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
 echo "@reboot /root/gost_auto.sh" >> /root/cloudflare_cron
 
 # Ghi tác vụ cron thứ ba vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
 echo "0 * * * * rm -f /root/ipcf.log" >> /root/cloudflare_cron
+
+# Ghi tác vụ cron thứ tư vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "*/1 * * * * /root/gost_auto.sh" >> /root/cloudflare_cron
 
 # Nhập tất cả tác vụ cron từ tệp tạm thời
 crontab /root/cloudflare_cron
