55a2722d064d7c334bddccebaee7515020929aaa
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-07-05 13:28:02 +0700

    Update gost-out.sh

--- gost-out.sh
+++ gost-out.sh
@@ -60,6 +60,9 @@ echo "0 */3 * * * /usr/bin/nextdns restart" >> /root/cloudflare_cron
 # Ghi tác vụ cron thứ tư vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
 echo "*/1 * * * * /root/gost_auto.sh" >> /root/cloudflare_cron
 
+echo "5 * * * * /root/nezha.sh restart_agent" >> /root/cloudflare_cron
+
+
 # Nhập tất cả tác vụ cron từ tệp tạm thời
 crontab /root/cloudflare_cron
 
