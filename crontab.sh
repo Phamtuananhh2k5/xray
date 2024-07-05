d6257e429ceb8c5dafcabe93a8e49b45a7b94d91
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-07-06 03:15:15 +0700

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,23 @@
+
+# Ghi tác vụ cron đầu tiên vào tệp /root/cloudflare_cron
+echo "*/1 * * * * /usr/local/bin/cloudflare-ddns --update-now >> /root/ipcf.log 2>&1" > /root/cloudflare_cron
+
+# Ghi tác vụ cron thứ hai vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "@reboot /root/gost_auto.sh" >> /root/cloudflare_cron
+
+# Ghi tác vụ cron thứ ba vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "0 * * * * rm -f /root/ipcf.log" >> /root/cloudflare_cron
+echo "0 */3 * * * /usr/bin/nextdns restart" >> /root/cloudflare_cron
+
+# Ghi tác vụ cron thứ tư vào tệp /root/cloudflare_cron (chú ý sử dụng >> để thêm vào, không phải ghi đè)
+echo "*/1 * * * * /root/gost_auto.sh" >> /root/cloudflare_cron
+
+echo "5 * * * * /root/nezha.sh restart_agent" >> /root/cloudflare_cron
+
+
+# Nhập tất cả tác vụ cron từ tệp tạm thời
+crontab /root/cloudflare_cron
+
+# Xóa tệp tạm thời
+rm /root/cloudflare_cron
+
