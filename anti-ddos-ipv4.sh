bb739aae162b4b02d8832a86586d05bdbcb22039
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-07-20 16:43:56 +0700

    Update anti-ddos-ipv4.sh

--- anti-ddos-ipv4.sh
+++ anti-ddos-ipv4.sh
@@ -54,6 +54,5 @@ sudo iptables -t security -F
 sudo iptables -t security -X
 EOF' && \
 sudo chmod +x /etc/anti-ddos/script.sh && \
-(crontab -l ; echo "0 * * * * /etc/anti-ddos/script.sh") | sudo crontab - && \
-(crontab -l ; echo "@reboot /etc/anti-ddos/run_check_ddos.sh") | sudo crontab -
+(crontab -l ; echo "0 * * * * /etc/anti-ddos/script.sh") | sudo crontab - 
 
