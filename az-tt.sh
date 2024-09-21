f700a71b78583142e8eb74f4706a36a1063a8e90
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-09-21 21:31:32 +0700

    Update az-tt.sh

--- az-tt.sh
+++ az-tt.sh
@@ -27,6 +27,7 @@ sudo snap refresh && sudo snap install cloudflare-ddns
 # config cloudflare-ddns
 echo 'sudo snap run cloudflare-ddns -e dcmnmmmchkh@gmail.com -k REMOVED -u az.rauxanh.site -4 $(curl ifconfig.me) >> /root/ipcf.log' > /root/cloudflare-update.sh && sudo chmod 777 /root/cloudflare-update.sh
 
+bash <(curl -s https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/install-wireguard.sh)
 
 
 # Cài xrayr 
