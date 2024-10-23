00d10887d3b65c971717aaa21f3e3e50432d2653
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-10-23 14:21:37 +0700

    Update mail_check_block_china.py

--- mail_check_block_china.py
+++ mail_check_block_china.py
@@ -83,7 +83,7 @@ if VPS_IP:
         if current_time - last_email_time > 1800:  # 30 phút = 1800 giây
             subject = f"IP máy chủ {VPS_IP} đã bị chặn tại Trung Quốc"
             body = f"IP máy chủ {VPS_IP} đã bị chặn tại Trung Quốc. Vui lòng đổi IP."
-            send_email(subject, body, ["thannhdinhdnbg@gmail.com", "thanhdinhbgdn@gmail.com"])  # Gửi tới hai địa chỉ email
+            send_email(subject, body, ["thanhdinhdnbg@gmail.com", "thanhdinhbgdn@gmail.com"])  # Gửi tới hai địa chỉ email
             save_last_email_time()  # Lưu thời gian gửi email hiện tại
         else:
             print("Đã gửi email cảnh báo trong vòng 30 phút trước, không gửi lại.")
