1a481bc0624fdc698e0b3c83a1b121a305c653e7
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2024-11-13 09:56:24 +0700

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -52,6 +52,11 @@ bash <(curl -fsSL https://github.com/go-gost/gost/raw/master/install.sh) --insta
 curl -o /root/check_status-domain_aws1.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws1.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws1.py") | crontab -
 curl -o /root/check_status-domain_aws2.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws2.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws2.py") | crontab -
 curl -o /root/check_status-domain_aws3.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws3.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws3.py") | crontab -
+curl -o /root/check_status-domain_aws4.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws4.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws4.py") | crontab -
+curl -o /root/check_status-domain_aws5.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws5.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws5.py") | crontab -
+curl -o /root/check_status-domain_aws6.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws6.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws6.py") | crontab -
+curl -o /root/check_status-domain_aws7.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws7.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws7.py") | crontab -
+
 # mail check ip block
 curl -o /root/mail_check_block_china.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/mail_check_block_china.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/mail_check_block_china.py") | crontab -
 # tele check ip block
