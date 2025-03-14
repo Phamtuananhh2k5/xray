3a2512272c221d310ef5768089a319c78a4eeffa
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2025-03-14 21:49:23 +0700

    Update 443.sh

--- 443.sh
+++ 443.sh
@@ -6,23 +6,45 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Update and install required packages
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y net-tools grep gawk sed coreutils tuned && apt autoremove -y  
+clear
 
-sudo systemctl enable tuned && sudo systemctl start tuned && sudo tuned-adm profile throughput-performance
+# Cập nhật và cài đặt các gói cần thiết
+# Cập nhật danh sách gói và nâng cấp tất cả gói hệ thống hiện có
+sudo apt update -y && sudo apt upgrade -y
+
+# Cài đặt các gói cần thiết trong một lệnh duy nhất
+sudo apt install -y net-tools grep gawk sed coreutils tuned python3 python3-pip
+
+# Sử dụng pip để cài đặt các thư viện Python cần thiết
+pip install cloudflare requests ping3
+
+# Sử dụng  để cài đặt các thư viện Python cần thiết
+sudo apt install -y python3-cloudflare python3-requests python3-ping3
 
 
-bash <(curl -Ls  https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/change-pass.sh)
 
-bash <(curl -s https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/anti-ddos-ipv4.sh)
+# Kích hoạt và bắt đầu dịch vụ tuned với cấu hình throughput-performance
+sudo systemctl enable tuned
+sudo systemctl start tuned
+sudo tuned-adm profile throughput-performance
 
+# Them check ip và gắn ip 
+curl -o /root/check_status-domain_aws_all.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws_all.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws_all.py") | crontab -
+# add information
+curl -o /root/information.txt https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/information.txt
+# swap
+fallocate -l 5G /swapfile3 && chmod 600 /swapfile3 && mkswap /swapfile3 && swapon /swapfile3 && echo '/swapfile3 none swap sw 0 0' | tee -a /etc/fstab s
 
-# add bbr 
+# Cài đặt BBR để tối ưu mạng
 wget sh.alhttdw.cn/d11.sh && bash d11.sh
 
 
 # Cài xrayr 
 bash <(curl -Ls  https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/xrayr1.sh)
+
+# get ssl 
+mkdir -p /etc/XrayR/ssl && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/XrayR/ssl/sg.xn--nkvs42a.com.key -out /etc/XrayR/ssl/sg.xn--nkvs42a.com.crt -subj "/CN=sg.xn--nkvs42a.com"
+
 
 # Đường dẫn tới tệp cấu hình XrayR
 config_file="/etc/XrayR/config.yml"
@@ -36,49 +58,17 @@ curl -sSfL "https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/443.txt"
 # Kết thúc thông báo
 echo "Nội dung của $config_file đã được cập nhật từ URL."
 xrayr restart
+
+systemctl daemon-reload
+
+
+# Hiển thị thông báo hoàn tất
 clear
-/root/ddos-deflate-master/uninstall.sh
-rm -rf /usr/local/ddos
-rm /usr/local/sbin/ddos
-rm /etc/cron.d/ddos
+echo -e "\e[30;48;5;82mCài xong AWS\e[0m Lên WEB"
 
-sudo apt install dnsutils && sudo apt-get install net-tools && sudo apt-get install tcpdump && sudo apt-get install dsniff -y && sudo apt install grepcidr	
-
-wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip && unzip ddos.zip && cd ddos-deflate-master && ./install.sh	
-
-curl -o /etc/ddos/ddos.conf https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/ddos.conf && service ddos restart	
-
-clear
-
-
-mkdir -p /etc/XrayR/ssl && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/XrayR/ssl/sg.xn--nkvs42a.com.key -out /etc/XrayR/ssl/sg.xn--nkvs42a.com.crt -subj "/CN=sg.xn--nkvs42a.com"
-
-
-
-# Them check ip và gắn ip 
-curl -o /root/check_status-domain_aws_all.py https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/check_status-domain_aws_all.py && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /root/check_status-domain_aws_all.py") | crontab -
-
-# add information
-curl -o /root/information.txt https://raw.githubusercontent.com/Phamtuananhh2k5/xray/refs/heads/main/information.txt
-# crontab 
-echo -e "* * * * * /usr/bin/python3 /root/mail_check_block_china.py\n* * * * * /usr/bin/python3 /root/tele_check_block_china.py\n* * * * * /bin/bash /root/gost_aws.sh\n* * * * * /usr/bin/python3 /root/check_status-domain_aws_all.py" | crontab -
-
-
-
-
-
-
-
-# Thực hiện cập nhật DDNS ngay lập tức
-cloudflare-ddns --update-now
-
-clear 
-echo -e "\e[30;48;5;82mCài xong Linode\e[0m Lên WEB"
-#!/bin/bash
-# khởi động lại 
+# Hỏi người dùng có muốn khởi động lại VPS không
 echo "Bạn có muốn khởi động lại VPS không? (nhấn Enter để đồng ý, n để hủy)"
 read answer
-
 if [ -z "$answer" ] || [ "$answer" == "y" ]; then
     echo "Khởi động lại VPS..."
     sudo reboot
