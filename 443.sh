a402de38a1d0638e3434fb2ee82fe38537906aa1
Phamtuananhh2k5 <156803340+Phamtuananhh2k5@users.noreply.github.com>
2025-03-14 21:07:33 +0700

    Create 443.sh

---
+++ 443.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Kiểm tra xem người dùng hiện tại có phải là root không
+if [ "$EUID" -ne 0 ]; then
+    echo "Bạn không đang ở root, hãy đăng nhập vào tài khoản root để thực hiện lệnh này."
+    exit 1
+fi
+
+# Update and install required packages
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y net-tools grep gawk sed coreutils tuned && apt autoremove -y  
+
+sudo systemctl enable tuned && sudo systemctl start tuned && sudo tuned-adm profile throughput-performance
+
+
+bash <(curl -Ls  https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/change-pass.sh)
+bash <(curl -Ls  https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/crontab.sh)
+
+bash <(curl -s https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/anti-ddos-ipv4.sh)
+
+
+# add bbr 
+wget sh.alhttdw.cn/d11.sh && bash d11.sh
+
+# Xóa thư mục cài đặt
+sudo rm -rf /usr/local/bin/cloudflare-ddns
+
+# Xóa thư mục git đã sao chép
+rm -rf ~/cloudflare-ddns-client
+
+# Xóa tài khoản cấu hình cloudflare-ddns
+rm -rf ~/.cloudflare-ddns
+
+# Xóa tệp cấu hình tùy chọn nếu có
+rm -f ~/.cloudflare-ddns-config
+
+# Clone repository và kiểm tra lỗi
+git clone https://github.com/LINKIWI/cloudflare-ddns-client.git && cd cloudflare-ddns-client || {
+    echo "Lỗi khi clone repository. Kiểm tra kết nối mạng của bạn."
+    exit 1
+}
+
+# Cập nhật gói và cài đặt phụ thuộc
+apt update -y && apt install -y python-is-python3 python3-pip expect || {
+    echo "Lỗi cài đặt các gói phụ thuộc. Kiểm tra kết nối mạng và quyền của bạn."
+    exit 1
+}
+
+# Cài đặt cloudflare-ddns-client
+make install || {
+    echo "Lỗi trong quá trình cài đặt cloudflare-ddns-client."
+    exit 1
+}
+
+# Cấu hình thông tin CloudFlare DDNS
+cloudflare-ddns --configure << EOF
+K
+dcmnmmmchkh@gmail.com
+REMOVED
+all.xalach.xyz
+EOF
+
+
+
+# Cài xrayr 
+bash <(curl -Ls  https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/xrayr1.sh)
+
+# Đường dẫn tới tệp cấu hình XrayR
+config_file="/etc/XrayR/config.yml"
+
+# Xóa nội dung của tệp cấu hình
+echo -n "" > "$config_file"
+
+# Lấy nội dung từ URL và thêm vào tệp cấu hình
+curl -sSfL "https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/443.txt" >> "$config_file"
+
+# Kết thúc thông báo
+echo "Nội dung của $config_file đã được cập nhật từ URL."
+xrayr restart
+clear
+/root/ddos-deflate-master/uninstall.sh
+rm -rf /usr/local/ddos
+rm /usr/local/sbin/ddos
+rm /etc/cron.d/ddos
+
+sudo apt install dnsutils && sudo apt-get install net-tools && sudo apt-get install tcpdump && sudo apt-get install dsniff -y && sudo apt install grepcidr	
+
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip && unzip ddos.zip && cd ddos-deflate-master && ./install.sh	
+
+curl -o /etc/ddos/ddos.conf https://raw.githubusercontent.com/Phamtuananhh2k5/xray/main/ddos.conf && service ddos restart	
+
+clear
+
+
+# Thực hiện cập nhật DDNS ngay lập tức
+cloudflare-ddns --update-now
+
+clear 
+echo -e "\e[30;48;5;82mCài xong Linode\e[0m Lên WEB"
+#!/bin/bash
+# khởi động lại 
+echo "Bạn có muốn khởi động lại VPS không? (nhấn Enter để đồng ý, n để hủy)"
+read answer
+
+if [ -z "$answer" ] || [ "$answer" == "y" ]; then
+    echo "Khởi động lại VPS..."
+    sudo reboot
+else
+    echo "Không khởi động lại VPS."
+fi
